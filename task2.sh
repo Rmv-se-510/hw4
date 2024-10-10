846d068b1f027fd117fd5b5a2e273a0262d22662
Meet Vora <meetvora090201@gmail.com>
2024-10-10 19:25:20 +0530

    task 2a modified

--- task2.sh
+++ task2.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
+cd dataset1
+
 # Step a: Find files having word "sample" and having atleast 3 occurences of "CSC510"
-files=$(grep -rl "sample" dataset1 | xargs  grep -o "CSC510" | uniq -c | grep -E -v "(1 )|(2 )" | grep -oE "dataset1/file_[0-9]+")
-
-# Step b: Find the files in sorted order, i.e. by count of word "CSC510" in descending order and then by size
-# Using stable sort concept, we will first sort the files by their size and then count of occurence in descending order
-sorted_file=$(echo $files | xargs ls -l | sort -k5,5 | grep -oE "dataset1/file_[0-9]+" |  xargs grep -o "CSC510" | uniq -c | sort -k1,1nr | grep -oE "dataset1/file_[0-9]+")
-echo $sorted_file
-
-
-sorted_file=$(echo $files | xargs ls -l | gawk '{ print $5, $9 }' | xargs -I{} bash -c 'count=$(grep -o "CSC510" {} | wc -l); echo $count $(ls -l {} | gawk "{print \$5}") {}' | sort -k1,1 -k2,2nr)
-# Print the sorted file list
-echo $sorted_file
+files=$(grep -rl "sample" | xargs  grep -o "CSC510" | uniq -c | grep -E -v "(1 )|(2 )" | grep -oE "file_[0-9]+")
+echo "Output from Steps: $files"
