4c2b49750438d642ca2c400869761bd31e8d9d53
vihar2712 <viharshah222@gmail.com>
2024-10-10 10:10:28 -0400

    added task2 c part

--- task2.sh
+++ task2.sh
@@ -4,10 +4,15 @@ cd dataset1
 
 # Step a: Find files having word "sample" and having atleast 3 occurences of "CSC510"
 files=$(grep -rl "sample" | xargs  grep -o "CSC510" | uniq -c | grep -E -v "(1 )|(2 )" | grep -oE "file_[0-9]+")
-echo "Output from Step a: $files"
+echo "Output from Step a: "
+echo $files
 
 # Step b: Find the files in sorted order, i.e. by count of word "CSC510" in descending order and then by size
 # Using stable sort concept, we will first sort the files by their size and then count of occurence in descending order
 sorted_file=$(echo $files | xargs ls -l | sort -k5,5 | grep -oE "file_[0-9]+" |  xargs grep -o "CSC510" | uniq -c | sort -k1,1nr | grep -oE "file_[0-9]+")
-echo "Output from Step b: $sorted_file"
+echo "Output from Step b: "
+echo $sorted_file
 
+# Step c: replace file_ with filtered_
+sub_file=$(echo $sorted_file | sed "s/file_/filtered_/g")
+echo "Output from Step c: "$sub_file
