3c2dcc627f3ab443819eeb02ba865d8631d2654e
Raj <81612989+Raj-mac24@users.noreply.github.com>
2024-10-10 14:27:02 -0700

    Update task3.sh
    Updated STEP-A,B,C

--- task3.sh
+++ task3.sh
@@ -12,6 +12,21 @@
 stepa=$(gawk -F, 'NR >= 2 && $3 == 2 && $13 ~ /S/ {print $0}' titanic.csv)
 echo "$stepa"
 
+# STEP B
+# replace male with M and replace female with F
+# we will use sed command for subsitution.
+# sicne there are multiple replacements we will use -e flag
+stepb=$(echo "$stepa" | sed -e 's/,male,/,M,/' -e 's/,female,/,F,/')
+#echo "$stepb"
+
+#STEP C
+# We want to calculate average age of the passengers from step b
+# First we will initialize the avg_age and count variable as 0 
+# then we will check for the condition whether age is missing or not.
+# if it is not missing increment counter, sum the age and print that line
+# finally check if the count is not 0, if not print average age.
+stepc=$(echo "$stepb" |  gawk -F, 'BEGIN {count = 0; sum_age = 0} {if ($7 != "") {count+=1; sum_age+=$7; print $0}} END {if (count > 0) print "Average age: " sum_age/count; else print "No valid data"}')
+echo "$stepc"
 
 
 
