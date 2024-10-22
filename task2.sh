db9afa249e07aedadacf0ce0b969ae57b91b985d
Meet Vora <72354237+Meet090201vora@users.noreply.github.com>
2024-10-22 00:09:21 +0000

    modified task2.sh to show just filename

--- task2.sh
+++ task2.sh
@@ -9,7 +9,7 @@ files=$(grep -rl "sample" | xargs  grep -o "CSC510" | uniq -c | grep -E -v "(1 )
 
 # Step b: Find the files in sorted order, i.e. by count of word "CSC510" in descending order and then by size
 # Using stable sort concept, we will first sort the files by their size and then count of occurence in descending order
-sorted_file=$(echo "$files" | xargs ls -l | sort -k5r | gawk '{print $9}' | xargs grep -o "CSC510" | uniq -c | sort -k1r | gawk '{print $2}')
+sorted_file=$(echo "$files" | xargs ls -l | sort -k5r | gawk '{print $9}' | xargs grep -o "CSC510" | uniq -c | sort -k1r | gawk '{print $2}' | cut -d: -f1)
 # echo "Output from Step b: "
 # echo "$sorted_file"
 
